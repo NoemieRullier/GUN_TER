f07e0f39f9c7697e91ba11cc9ad58163595d4e28
Jesus Patate <g.a.e.t.a.n@orange.fr>
2013-02-20 12:22:44 +0100

    Ajout d'une methode de requete sur la traduction en rdf des donnees

--- Wrappers/WrapperActivites/src/Wrapper.java
+++ Wrappers/WrapperActivites/src/Wrapper.java
@@ -1,25 +1,39 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.sparql.core.TriplePath;
+import com.hp.hpl.jena.sparql.syntax.ElementGroup;
+import com.hp.hpl.jena.sparql.syntax.ElementPathBlock;
+import com.hp.hpl.jena.util.FileManager;
 
 
 public class Wrapper {
@@ -27,8 +41,8 @@ public class Wrapper {
     private static final String ONTO_PREFIX = "onto";
     private static final String ONTO_URL = "http://example.org/";
 
-    private static final String OUTPUT_XML = "activities.xml"; 
-    private static final String OUTPUT_RDF = "activities.ttl";
+    private static final String LOCAL_DATASET = "res/activities.xml";
+    private static final String RDF_FILE = "res/activities.ttl";
 
     private static final String DATA_URL =
 	    "http://data.nantes.fr/api/publication/"
@@ -41,60 +55,90 @@ public class Wrapper {
     // Only for convenience...
     private static HashMap<String, String> mapping = 
 	    new HashMap<String, String>();
-    
-    // Useless ?
-    private static enum Field {ID, NAME, ADDRESS1, ADDRESS2, POSTAL_CODE, TOWN,
-	WEBSITE, MAIL, V_IMPAIRMENT, H_IMPAIRMENT};
 
-	public Document getXml() throws IOException, JDOMException {
+    /*
+    // Useless ?
+    private static enum Field {ID, NAME, ADDRESS1, ADDRESS2, POSTAL_CODE, TOWN, 
+	WEBSITE, MAIL, V_IMPAIRMENT, H_IMPAIRMENT};
+     */
+
+    private Model model_ = ModelFactory.createDefaultModel();
+
+    /**
+     * 
+     * 
+     * @return
+     * @throws IOException
+     * @throws JDOMException
+     */
+    public Document getXml() throws IOException, JDOMException {
+	InputStream stream = null;
+
+	try {
 	    URL url = new URL(DATA_URL);
 	    URLConnection connection = url.openConnection();
 
-	    InputStreamReader stream =
-		    new InputStreamReader(connection.getInputStream());
-
-	    return new SAXBuilder().build(stream);
+	    stream = connection.getInputStream();
 	}
 
-	public void writeXml() throws JDOMException, IOException {
-	    XMLOutputter sortie =
-		    new XMLOutputter(Format.getPrettyFormat());
-	    sortie.output(this.getXml(),
-		    new FileOutputStream(new File(OUTPUT_XML)));
-	}
-	
-	private void loadMapping() throws FileNotFoundException, IOException {
-	    Properties prop = new Properties();
-	    prop.load(new FileReader(MAPPING_FILE));
-	    
-	    for(Object key : prop.keySet()) {
-		mapping.put(key.toString(), prop.getProperty(key.toString()));
+	catch(IOException e) {
+	    File local = new File(LOCAL_DATASET);
+
+	    if(local.exists()) {
+		stream = new FileInputStream(local);
+	    }
+
+	    else {
+		throw new RuntimeException("Récupération des données échouée.");
 	    }
 	}
-	
-	public void toRdf() throws IOException, JDOMException {
-	    Model model = ModelFactory.createDefaultModel();
-	    model.setNsPrefix(ONTO_PREFIX, ONTO_URL);
+
+	return new SAXBuilder().build(stream);
+    }
+
+    /*
+    public void writeXml() throws JDOMException, IOException {
+	XMLOutputter sortie =
+		new XMLOutputter(Format.getPrettyFormat());
+	sortie.output(this.getXml(),
+		new FileOutputStream(new File(OUTPUT_XML)));
+    }
+     */
+
+    private void loadMapping() throws FileNotFoundException, IOException {
+	Properties prop = new Properties();
+	prop.load(new FileReader(MAPPING_FILE));
+
+	for(Object key : prop.keySet()) {
+	    mapping.put(key.toString(), prop.getProperty(key.toString()));
+	}
+    }
+
+    public void toRdf() throws IOException, JDOMException {
+	File rdfFile = new File(RDF_FILE);
+
+	if(rdfFile.exists() == false) {
+	    model_.setNsPrefix(ONTO_PREFIX, ONTO_URL);
 
 	    Document xml = this.getXml();
 	    Element root = xml.getRootElement();
 
 	    List<Element> listeActi = root.getChild("data").getChildren("element");
-	    
+
 	    // Every <element> matches with an activity
 	    for(Element activity : listeActi) {
 		Resource res = null;
-		
+
 		// Every java property matches with an RDF property
 		for(String key : mapping.keySet()) {
 		    Element current = activity;
 
 		    String value = mapping.get(key);
-		    
+
 		    // Several elements (paths) which fit an RDF property
 		    // are separated by a coma
 		    String[] paths = value.split(",");
-		    
+
 		    for(int p = 0 ; p < paths.length ; p++) {
 			String path = paths[p];
 			String[] elements = path.split("\\.");
@@ -111,37 +155,92 @@ public class Wrapper {
 				    String id = text.replace(' ', '_')
 					    .replace('\'', '_');
 
-				    res = model.createResource(ONTO_URL + id);
+				    res = model_.createResource(ONTO_URL + id);
 				}
-				
+
 				res.addProperty(
-					model.createProperty(ONTO_URL, key),
+					model_.createProperty(ONTO_URL, key),
 					text);
 			    }
 			}
 		    }
 		}
 	    }
+
+	    model_.write(new FileOutputStream(RDF_FILE));
+	}
+
+	else {
+	    InputStream input = FileManager.get().open(RDF_FILE);
+	    model_.read(input, "TURTLE");
+	}
+    }
+
+    public Model query1(String queryString) throws IOException, JDOMException {
+	Model resModel = ModelFactory.createDefaultModel();
+	
+	toRdf();
+
+	Query query = QueryFactory.create(queryString);
+	QueryExecution qexec = QueryExecutionFactory.create(query, model_);
+	ResultSet results = qexec.execSelect();
+	
+	PrefixMapping pm = query.getPrefixMapping();
+	Map<String, String> truc = pm.getNsPrefixMap();
+	
+	for(String key : truc.keySet()) {
+	    resModel.setNsPrefix(key, truc.get(key));
+	}
+	
+	ElementGroup eltGroup = (ElementGroup) query.getQueryPattern();
+	ElementPathBlock pathBlock =
+		(ElementPathBlock) eltGroup.getElements().get(0);
+	
+	while(results.hasNext())
+	{
+	    QuerySolution sol = results.next();
 	    
-	    model.write(new FileOutputStream(OUTPUT_RDF));
-	}
-
-	public static void main(String[] args) {
-	    Wrapper w = new Wrapper();
-
-	    try {
-		w.loadMapping();
-		w.toRdf();
-	    }
-
-	    catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-
-	    catch (JDOMException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	    Iterator<TriplePath> it = pathBlock.patternElts();
+	    
+	    Node s;
+	    Node o;
+	    Resource res;
+	    Property prop;
+	    
+	    while(it.hasNext()) {
+		TriplePath queryTriple = it.next();
+		
+		s = queryTriple.getSubject();
+		o = queryTriple.getObject();
+		
+		res = resModel.createResource(sol.getResource(s.getName()).getURI());
+		prop = resModel.createProperty(queryTriple.getPredicate().getURI());
+		
+		res.addProperty(prop, o.getName());
 	    }
 	}
+	
+	return resModel;
+    }
+    
+    public static void main(String[] args) {
+	Wrapper w = new Wrapper();
+
+	try {
+	    w.loadMapping();
+	    Model m1 = w.query1("prefix onto: <http://example.org/> SELECT ?x ?ad WHERE{ ?x onto:hasAddress ?ad}");
+	    
+	    m1.write(System.out);
+	}
+
+	catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	catch (JDOMException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
 }
